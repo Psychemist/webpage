56482a2220f6f2752bd216139bffe73f58284692
Mike Chan <kiwai0929@gmail.com>
2022-12-23 21:11:31 +0800

    enhance CSS

--- src/static/SideBar.tsx
+++ src/static/SideBar.tsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import '../css/Sidebar.css'
 import { Link } from 'react-router-dom';
-
-
+import { FaHome, FaInfoCircle, FaSearch, FaThList, FaBookmark, FaBullhorn } from 'react-icons/fa';
 
 
 export default function SideBar() {
   return (
     <nav className="sidebar">
       <div className="sidebar-icon-container">
-        <Link to="home" className="link" > 主頁 </Link>
-        <Link to="about" className="link" > 關於我們 </Link>
-        <Link to="activity" className="link" > 活動搜尋 </Link>
-        <Link to="application" className="link" > 我的申請 </Link>
-        <Link to="bookmark" className="link" > 書籤 </Link>
-        <Link to="news" className="link" > 最新消息 </Link>
-        <Link to="cms" className="link" > 進入CMS </Link>
+        <Link to="home" className="link"><FaHome className="icon" /><span>主頁</span> </Link>
+        <Link to="about" className="link"><FaInfoCircle className="icon" /><span>關於我們</span> </Link>
+        <Link to="activity" className="link"><FaSearch className="icon" /><span>活動搜尋</span> </Link>
+        <Link to="application" className="link"><FaThList className="icon" /><span>我的申請</span> </Link>
+        <Link to="bookmark" className="link"><FaBookmark className="icon" /><span>書籤</span> </Link>
+        <Link to="news" className="link"><FaBullhorn className="icon" /><span>最新消息</span> </Link>
+        <Link to="cms" className="link"><span>進入CMS</span> </Link>
       </div>
 
     </nav>
